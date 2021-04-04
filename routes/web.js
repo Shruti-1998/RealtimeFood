1e8c704119e1181eca7f76af8a9a8fd217deab12
pournima <shrutibambal98@gmail.com>
2021-04-04 15:43:06 +0530

    'add_orders_table,_place_order'

--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,13 @@
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
 const homeController = require('../app/http/controllers/homeController')
+const orderController = require('../app/http/controllers/customers/orderController')
+const AdminOrderController = require('../app/http/controllers/admin/orderController')
+
+//middlewares
 const guest = require('../app/http/middleware/guest')
+const auth = require('../app/http/middleware/auth')
+const admin = require('../app/http/middleware/admin')
 
 function initRoutes(app) {
     app.get('/', homeController().index)
@@ -14,7 +20,14 @@ function initRoutes(app) {
 
     app.get('/cart', cartController().index)
     app.post('/update-cart', cartController().update) 
+
     
+    //customer routes
+    app.post('/orders', auth, orderController().store)
+    app.get('/customers/orders', auth, orderController().index)
+
+    //Admin routes
+    app.get('/admin/orders', admin, AdminOrderController().index)
 
 }
 
